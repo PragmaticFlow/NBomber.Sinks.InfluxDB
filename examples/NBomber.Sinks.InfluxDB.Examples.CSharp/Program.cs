85b641de7261477686b1a3ee9355361bebd5f891
Antya Dev <antyadev@gmail.com>
2019-02-12 23:27:19 +0200

    initial commit

---
+++ examples/NBomber.Sinks.InfluxDB.Examples.CSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Http.CSharp;
+
+namespace NBomber.Sinks.InfluxDB.Examples.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var influxDb = new InfluxDBSink(url: "http://localhost:8086", dbName: "default");
+
+            var scenario = BuildScenario();
+            NBomberRunner.RegisterScenarios(scenario)
+                         .SaveStatisticsTo(influxDb)
+                         .RunInConsole();
+        }
+
+        static Scenario BuildScenario()
+        {
+            var step = HttpStep.CreateRequest("GET", "https://www.youtube.com")                               
+                               .WithHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                               .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")                               
+                               .BuildStep("GET request");
+
+            return ScenarioBuilder.CreateScenario("youtube_scenario", step)
+                .WithConcurrentCopies(10)
+                .WithDuration(TimeSpan.FromSeconds(10));
+        }
+    }
+}
---
+++ src/NBomber.Sinks.InfluxDB/InfluxDBSink.fs
@@ -0,0 +1,31 @@
+namespace NBomber.Sinks.InfluxDB
+
+open System.Threading.Tasks
+open App.Metrics
+open App.Metrics.Gauge
+open NBomber.Contracts
+
+type InfluxDBSink(url: string, dbName: string) = 
+
+    let metrics = MetricsBuilder().Report.ToInfluxDb(url, dbName).Build()
+
+    let saveGaugeMetrics (s: Statistics) =
+        let contextName = sprintf "%s_%s" s.ScenarioName s.StepName
+
+        [("OkCount", float s.OkCount); ("FailCount", float s.FailCount); 
+         ("RPS", float s.RPS); ("Min", float s.Min); ("Mean", float s.Mean); ("Max", float s.Max)            
+         ("Percent50", float s.Percent50); ("Percent75", float s.Percent75); ("Percent95", float s.Percent95); ("StdDev", float s.StdDev)                 
+         ("DataMinKb", s.DataMinKb); ("DataMeanKb", s.DataMeanKb); ("DataMaxKb", s.DataMaxKb); ("AllDataMB", s.AllDataMB)]        
+
+        |> List.iter(fun (name, value) -> 
+            let m = GaugeOptions(
+                        Name = name,
+                        Context = contextName, 
+                        Tags = MetricTags([|"nodeName"; "sender"|], 
+                                          [|s.Meta.NodeName; s.Meta.Sender.ToString()|]))
+            metrics.Measure.Gauge.SetValue(m, value))
+
+    interface IStatisticsSink with
+        member x.SaveStatistics (statistics: Statistics[]) =        
+            statistics |> Array.iter(saveGaugeMetrics)
+            Task.WhenAll(metrics.ReportRunner.RunAllAsync())  
