b8913291f494b9bfd31549933c93308777bbc1b3
Boris Golubenko <boris.go@sbtech.com>
2019-04-04 15:32:00 +0300

    add custom tags support

--- src/NBomber.Sinks.InfluxDB/InfluxDBSink.fs
+++ src/NBomber.Sinks.InfluxDB/InfluxDBSink.fs
@@ -1,28 +1,42 @@
 namespace NBomber.Sinks.InfluxDB
 
+open System
+open System.Runtime.InteropServices
 open System.Threading.Tasks
 open App.Metrics
 open App.Metrics.Gauge
 open NBomber.Contracts
 
-type InfluxDBSink(url: string, dbName: string) = 
+type ScenarioName = string
+
+type StepName = string
+
+type InfluxDBSink(url: string, dbName: string,
+                  [<Optional;DefaultParameterValue(null: Func<ScenarioName, StepName, MetricTags>)>]
+                  getTagsFn: Func<ScenarioName, StepName, MetricTags>) =
 
     let metrics = MetricsBuilder().Report.ToInfluxDb(url, dbName).Build()
+
+    let getTags(s: Statistics) =
+        let defaultTags = MetricTags([|"nodeName"; "sender"|],
+                                     [|s.Meta.NodeName; s.Meta.Sender.ToString()|])
+        if isNull(getTagsFn)
+        then defaultTags
+        else MetricTags.Concat(defaultTags, getTagsFn.Invoke(s.ScenarioName, s.StepName))
 
     let saveGaugeMetrics (s: Statistics) =
         let contextName = sprintf "%s_%s" s.ScenarioName s.StepName
 
-        [("OkCount", float s.OkCount); ("FailCount", float s.FailCount); 
-         ("RPS", float s.RPS); ("Min", float s.Min); ("Mean", float s.Mean); ("Max", float s.Max)            
-         ("Percent50", float s.Percent50); ("Percent75", float s.Percent75); ("Percent95", float s.Percent95); ("StdDev", float s.StdDev)                 
-         ("DataMinKb", s.DataMinKb); ("DataMeanKb", s.DataMeanKb); ("DataMaxKb", s.DataMaxKb); ("AllDataMB", s.AllDataMB)]        
+        [("OkCount", float s.OkCount); ("FailCount", float s.FailCount);
+         ("RPS", float s.RPS); ("Min", float s.Min); ("Mean", float s.Mean); ("Max", float s.Max)
+         ("Percent50", float s.Percent50); ("Percent75", float s.Percent75); ("Percent95", float s.Percent95); ("StdDev", float s.StdDev)
+         ("DataMinKb", s.DataMinKb); ("DataMeanKb", s.DataMeanKb); ("DataMaxKb", s.DataMaxKb); ("AllDataMB", s.AllDataMB)]
 
-        |> List.iter(fun (name, value) -> 
+        |> List.iter(fun (name, value) ->
             let m = GaugeOptions(
                         Name = name,
-                        Context = contextName, 
-                        Tags = MetricTags([|"nodeName"; "sender"|], 
-                                          [|s.Meta.NodeName; s.Meta.Sender.ToString()|]))
+                        Context = contextName,
+                        Tags = getTags(s))
             metrics.Measure.Gauge.SetValue(m, value))
 
     interface IStatisticsSink with
