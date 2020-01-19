3347ee002361b06c89a1434531eca973c858b29d
Andreas Vilinski <vilinski@yahoo.de>
2020-01-19 19:50:54 +0100

    trim trailing spaces

--- src/NBomber.Sinks.InfluxDB/InfluxDBSink.fs
+++ src/NBomber.Sinks.InfluxDB/InfluxDBSink.fs
@@ -9,24 +9,24 @@ open Microsoft.Extensions.Configuration
 
 open NBomber.Contracts
 
-type InfluxDBSink(url: string, dbName: string) = 
+type InfluxDBSink(url: string, dbName: string) =
 
     let metrics = MetricsBuilder().Report.ToInfluxDb(url, dbName).Build()
 
     let saveGaugeMetrics (testInfo: TestInfo) (s: Statistics) =
-        
+
         let operation =
-            match s.NodeInfo.CurrentOperation with            
+            match s.NodeInfo.CurrentOperation with
             | NodeOperationType.Bombing  -> "bombing"
             | NodeOperationType.Complete -> "complete"
             | _                          -> "unknown_operation"
-            
-        [("OkCount", float s.OkCount); ("FailCount", float s.FailCount); 
-         ("RPS", float s.RPS); ("Min", float s.Min); ("Mean", float s.Mean); ("Max", float s.Max)            
-         ("Percent50", float s.Percent50); ("Percent75", float s.Percent75); ("Percent95", float s.Percent95); ("StdDev", float s.StdDev)                 
+
+        [("OkCount", float s.OkCount); ("FailCount", float s.FailCount);
+         ("RPS", float s.RPS); ("Min", float s.Min); ("Mean", float s.Mean); ("Max", float s.Max)
+         ("Percent50", float s.Percent50); ("Percent75", float s.Percent75); ("Percent95", float s.Percent95); ("StdDev", float s.StdDev)
          ("DataMinKb", s.DataMinKb); ("DataMeanKb", s.DataMeanKb); ("DataMaxKb", s.DataMaxKb); ("AllDataMB", s.AllDataMB)]
-        
-        |> List.iter(fun (name, value) -> 
+
+        |> List.iter(fun (name, value) ->
             let m = GaugeOptions(
                         Name = name,
                         Context = "NBomber",
@@ -41,17 +41,17 @@ type InfluxDBSink(url: string, dbName: string) =
     interface IReportingSink with
         member x.Init(logger: ILogger, infraConfig: IConfiguration option) =
             ()
-            
+
         member x.StartTest(testInfo: TestInfo) =
             Task.CompletedTask
-        
-        member x.SaveRealtimeStats (testInfo: TestInfo, stats: Statistics[]) =        
+
+        member x.SaveRealtimeStats (testInfo: TestInfo, stats: Statistics[]) =
             stats |> Array.iter(saveGaugeMetrics testInfo)
             Task.WhenAll(metrics.ReportRunner.RunAllAsync())
-            
+
         member x.SaveFinalStats(testInfo: TestInfo, stats: Statistics[], reportFiles: ReportFile[]) =
             stats |> Array.iter(saveGaugeMetrics testInfo)
             Task.CompletedTask
-            
+
         member x.FinishTest(testInfo: TestInfo) =
             Task.CompletedTask
