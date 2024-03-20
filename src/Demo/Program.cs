5d9d43ce5418191a8c64f4e662c5b679a11f98cf
Andrey <111135827+mangystx@users.noreply.github.com>
2024-03-20 17:06:42 +0200

    f

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,5 +1,7 @@
-﻿using NBomber.CSharp;
+﻿using InfluxDB.Client.Writes;
+using NBomber.CSharp;
 using NBomber.Sinks.InfluxDB;
+using TimeSpan = System.TimeSpan;
 
 new InfluxDBReportingExample().Run();
 
@@ -9,51 +11,44 @@ public class InfluxDBReportingExample
 
     public void Run()
     {
-        var scenario = Scenario.Create("user_flow_scenario", async context =>
+        var writeScenario = Scenario.Create("write_scenario", async context =>
             {
-                // here is an example how you can use InfluxDBSink to write your custom data.
-                // var writeApi = _influxDbSink.InfluxClient.GetWriteApiAsync();
-                //
-                // var point = PointData
-                //     .Measurement("nbomber")
-                //     .Field("my_custom_counter", 1);
-                //
-                // writeApi.WritePointAsync(point);
-
-                var step1 = await Step.Run("login", context, async () =>
-                {
-                    await Task.Delay(500);
-                    return Response.Ok(sizeBytes: 10, statusCode: "200");
-                });
-
-                var step2 = await Step.Run("get_product", context, async () =>
-                {
-                    await Task.Delay(1000);
-                    return Response.Ok(sizeBytes: 20, statusCode: "200");
-                });
-
-                var step3 = await Step.Run("buy_product", context, async () =>
-                {
-                    await Task.Delay(2000);
-                    return Response.Ok(sizeBytes: 30, statusCode: "200");
-                });
-
+                var writeApi = _influxDbSink.InfluxClient.GetWriteApiAsync();
+                
+                var point = PointData
+                    .Measurement("nbomber")
+                    .Field("my_custom_counter", 1);
+                
+                await writeApi.WritePointsAsync(Enumerable.Repeat(point, 5).ToArray());
+                
                 return Response.Ok(statusCode: "201");
             })
-            .WithWarmUpDuration(TimeSpan.FromSeconds(10))
+            .WithoutWarmUp()
             .WithLoadSimulations(
-                Simulation.RampingInject(rate: 200, interval: TimeSpan.FromSeconds(1),
-                    during: TimeSpan.FromMinutes(1)), // rump-up to rate 200
-                Simulation.Inject(rate: 200, interval: TimeSpan.FromSeconds(1),
-                    during: TimeSpan.FromSeconds(30)), // keep injecting with rate 200
-                Simulation.RampingInject(rate: 0, interval: TimeSpan.FromSeconds(1),
-                    during: TimeSpan.FromMinutes(1)) // rump-down to rate 0
+                Simulation.KeepConstant(20, TimeSpan.FromSeconds(30))
             );
 
+        var readScenario = Scenario.Create("read_scenario", async context =>
+            {
+                var query = @"
+                    from(bucket: ""nbomber"")
+                    |> range(start: -1m)
+                    ";
+                
+                var readApi = _influxDbSink.InfluxClient.GetQueryApi();
+                var results = await readApi.QueryAsync(query, "nbomber");
+            
+            return Response.Ok(statusCode: "200");
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.KeepConstant(20, TimeSpan.FromSeconds(30))
+        );
+        
         NBomberRunner
-            .RegisterScenarios(scenario)
+            .RegisterScenarios(writeScenario, readScenario)
             .LoadInfraConfig("infra-config.json")
-            .WithReportingInterval(TimeSpan.FromSeconds(5))
+            //.WithReportingInterval(TimeSpan.FromSeconds(5))
             .WithReportingSinks(_influxDbSink)
             .WithTestSuite("reporting")
             .WithTestName("influx_db_demo")
